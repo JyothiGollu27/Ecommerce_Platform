052e58b438af61e541242bfd7d1fd47f30632d36
Jyothi <2387556@cognizant.com>
2025-03-18 11:23:52 +0530

    commit

---
+++ Models/RegularUser.cs
@@ -0,0 +1,11 @@
+﻿namespace Ecommerce_Platform.Models
+{
+    public class RegularUser : User
+    {
+        public string? FirstName { get; set; }
+        public string? LastName { get; set; }
+        public string? PhoneNumber { get; set; }
+        public string? Address { get; set; }
+        public ICollection<Order>? Orders { get; set; }
+    }
+}
